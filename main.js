adf7d0dcf3a211fda1ac1bdba2fd5be58257d6ca
Saatvik Arya <aryasaatvik@gmail.com>
2018-07-07 14:49:03 -0500

    added coloring to overall and difficulty

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ function logProfessors() {
     var lectures = document.querySelectorAll("#crse-sections > div > div")
     lectures.forEach(function (lecture) {
         var professor = lecture.querySelector("div > div.panel-body > table > tbody > tr > td:nth-child(4) > a")
-        var table = lecture.querySelector("div > div.panel-body > table")
-        if (professor != null) {
+        var table = professor.parentNode.parentNode.parentNode.parentNode
+        if (professor) {
             console.log(professor.innerText)
             getRating(professor.innerText, table)
         }
@@ -43,29 +43,57 @@ function insertRatings(professorStats, table) {
     var tableHeader = table.querySelector('thead > tr')
 
     var overallHeader = document.createElement('th')
+    overallHeader.classList.add('rmpheader')
     overallHeader.innerText = "Overall"
     tableHeader.appendChild(overallHeader)
 
     var difficultyHeader = document.createElement('th')
+    difficultyHeader.classList.add('rmpheader')
     difficultyHeader.innerText = "Difficulty"
     tableHeader.appendChild(difficultyHeader)
 
     var totalHeader = document.createElement('th')
-    totalHeader.innerText = "#"
+    totalHeader.classList.add('rmpheader')
+    totalHeader.innerText = "# of Reviews"
     tableHeader.appendChild(totalHeader)
 
     var tableBody = table.querySelector('tbody > tr')
 
     var overall = document.createElement('td')
+    overall.classList.add('rmpcol')
     overall.innerText = professorStats.averageratingscore_rf
+    overall.style.color = getOverallColor(professorStats.averageratingscore_rf)
     tableBody.appendChild(overall)
 
     var difficulty = document.createElement('td')
+    difficulty.classList.add('rmpcol')
     difficulty.innerText = professorStats.averageeasyscore_rf
+    difficulty.style.color = getDifficultyColor(professorStats.averageeasyscore_rf)
     tableBody.appendChild(difficulty)
 
     var total = document.createElement('td')
+    total.classList.add('rmpcol')
     total.innerText = professorStats.total_number_of_ratings_i
     tableBody.appendChild(total)
 
+}
+
+function getOverallColor(num) {
+    if (num >= 3.0) {
+        return "#b4d235"
+    } else if (num >= 2.0) {
+        return "#f7cc20"
+    } else {
+        return "#df3d5f"
+    }
+}
+
+function getDifficultyColor(num) {
+    if (num >= 4.0) {
+        return "#df3d5f"
+    } else if (num >= 3.0) {
+        return "#f7cc20"
+    } else {
+        return "#b4d235"
+    }
 }
