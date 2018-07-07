7f44fac1100b28934166d881cd32b804afeaedcd
Saatvik Arya <aryasaatvik@gmail.com>
2018-07-07 16:33:18 -0500

    variable change

--- main.js
+++ main.js
@@ -19,12 +19,12 @@ function logProfessors() {
 
 function getRating(professor, table) {
     professor = professor.split(" ")
-    const rateMyProfessor = "https://search-a.akamaihd.net/typeahead/suggest/?solrformat=true&rows=1&q=" + professor[0] + "+" + professor[professor.length - 1] + "+AND+schoolid_s%3A1257&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&rows=20&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s+averageeasyscore_rf&fq="
-    console.log(rateMyProfessor)
+    const professorRatings = "https://search-a.akamaihd.net/typeahead/suggest/?solrformat=true&rows=1&q=" + professor[0] + "+" + professor[professor.length - 1] + "+AND+schoolid_s%3A1257&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&rows=20&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s+averageeasyscore_rf&fq="
+    console.log(professorRatings)
 
     var professorStats
     var request = new XMLHttpRequest()
-    request.open('GET', rateMyProfessor, true);
+    request.open('GET', professorRatings, true);
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             var data = JSON.parse(request.responseText)
